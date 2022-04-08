2b51dd0f3c10bc44d6d5a094a5eea1f7a11634ba
mja00 <teamnirvana2@gmail.com>
2022-04-08 12:15:07 -0400

    Persistent data fix

---
+++ src/main/java/dev/mja00/swarmsmps2/events/ModEvents.java
@@ -0,0 +1,36 @@
+package dev.mja00.swarmsmps2.events;
+
+import dev.mja00.swarmsmps2.SwarmsmpS2;
+import net.minecraft.Util;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.server.command.ConfigCommand;
+import org.apache.logging.log4j.Logger;
+
+import java.util.Objects;
+import java.util.UUID;
+
+@Mod.EventBusSubscriber(modid = SwarmsmpS2.MODID)
+public class ModEvents {
+
+    static Logger LOGGER = SwarmsmpS2.LOGGER;
+    static final UUID DUMMY = Util.NIL_UUID;
+
+    @SubscribeEvent
+    public static void onCommandsRegister(RegisterCommandsEvent event) {
+        ConfigCommand.register(event.getDispatcher());
+    }
+
+    @SubscribeEvent
+    public static void onPlayerCloneEvent(PlayerEvent.Clone event) {
+        if (!event.getOriginal().getLevel().isClientSide) {
+            event.getOriginal().getPersistentData().getAllKeys().forEach(key -> {
+               if(key.contains(SwarmsmpS2.MODID)) {
+                   event.getPlayer().getPersistentData().put(key, Objects.requireNonNull(event.getOriginal().getPersistentData().get(key)));
+               }
+            });
+        }
+    }
+}
