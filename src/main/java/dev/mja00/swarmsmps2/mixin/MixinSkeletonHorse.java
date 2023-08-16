de14a46b44bc040b21014c4bbd16a9b32bece140
mja00 <teamnirvana2@gmail.com>
2023-08-15 20:54:46 -0400

    fix: pick the right biome

--- src/main/java/dev/mja00/swarmsmps2/mixin/MixinSkeletonHorse.java
+++ src/main/java/dev/mja00/swarmsmps2/mixin/MixinSkeletonHorse.java
@@ -1,6 +1,7 @@
 package dev.mja00.swarmsmps2.mixin;
 
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.DifficultyInstance;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
@@ -18,7 +19,6 @@ import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.ServerLevelAccessor;
 import net.minecraft.world.level.biome.Biome;
-import net.minecraft.world.level.biome.Biomes;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -93,7 +93,7 @@ public abstract class MixinSkeletonHorse extends AbstractHorse {
         // Get the current biome
         Biome biome = pLevel.getBiome(this.blockPosition()).value();
         // Check if the biome is a taiga biome
-        if (biome != ForgeRegistries.BIOMES.getValue(Biomes.TAIGA.getRegistryName())) {
+        if (biome != ForgeRegistries.BIOMES.getValue(new ResourceLocation("swarmsmp:taiga"))) {
             // Don't spawn, aka kill the FUCK outta it
             this.actuallyHurt(DamageSource.ANVIL, 10000);
         }
