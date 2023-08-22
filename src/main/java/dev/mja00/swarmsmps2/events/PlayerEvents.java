91fc0010e2bf1254eb7b40709d38d52f4da51d6f
mja00 <teamnirvana2@gmail.com>
2023-08-21 20:06:46 -0400

    fix: modlist shit

--- src/main/java/dev/mja00/swarmsmps2/events/PlayerEvents.java
+++ src/main/java/dev/mja00/swarmsmps2/events/PlayerEvents.java
@@ -3,16 +3,21 @@ package dev.mja00.swarmsmps2.events;
 import com.mojang.blaze3d.vertex.PoseStack;
 import dev.mja00.swarmsmps2.SSMPS2Config;
 import dev.mja00.swarmsmps2.SwarmsmpS2;
+import dev.mja00.swarmsmps2.network.SwarmSMPPacketHandler;
+import dev.mja00.swarmsmps2.network.packets.ModListPacket;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.TitleScreen;
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.client.event.ClientPlayerNetworkEvent;
 import net.minecraftforge.client.event.ScreenEvent;
 import net.minecraftforge.client.event.ScreenOpenEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.forgespi.language.IModInfo;
 import org.apache.logging.log4j.Logger;
 
 import java.awt.*;
@@ -22,6 +27,7 @@ import java.lang.management.ManagementFactory;
 public class PlayerEvents {
 
     static Logger LOGGER = SwarmsmpS2.LOGGER;
+    public static boolean connected;
 
     // Startup time stuff
     public static long expectedTime = 0;
@@ -102,5 +108,21 @@ public class PlayerEvents {
             doneTime = startupTime;
             SSMPS2Config.addStartupTime(startupTime);
         }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerConnect(ClientPlayerNetworkEvent.LoggedInEvent event) {
+        // Send mod list to server
+        LOGGER.debug("Connected to server");
+        ModList modList = ModList.get();
+        String[] modNames = modList.getMods().stream().map(IModInfo::getModId).toArray(String[]::new);
+        ModListPacket packet = new ModListPacket(modNames);
+        SwarmSMPPacketHandler.MOD_LIST_CHANNEL.sendToServer(packet);
+    }
+
+    @SubscribeEvent
+    public static void onPlayerLogout(ClientPlayerNetworkEvent.LoggedOutEvent event) {
+        LOGGER.debug("Disconnected from server");
+        connected = false;
     }
 }
--- src/main/java/dev/mja00/swarmsmps2/network/packets/ModListPacket.java
+++ src/main/java/dev/mja00/swarmsmps2/network/packets/ModListPacket.java
@@ -6,13 +6,31 @@ import net.minecraftforge.network.NetworkEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.List;
 import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 // This is a packet that is sent to the server with a list of mods currently installed on the server
 // We'll use this to check if the player is using a modded client
 public class ModListPacket {
     // We'll just use a list of mod ids
     private final String[] modIds;
+    private static final String goodMods = """
+            dynamiclightsreforged,ftbessentials,advancedperipherals,additionalentityattributes,apoli,calio,origins,deleteitem,playeranimator,
+            additionalbanners,incontrol,connectivity,serverredirect,rubidium,hourglass,darkness,ctm,cookingforblockheads,controlling,placebo,
+            wildbackport,extendedslabs,extrapotions,bookshelf,consistency_plus,mcwdoors,balm,dynview,jeresources,cloth_config,shetiphiancore,
+            flytre_lib,lod,taterzens,config2brigadier,ambientsounds,mcwtrpdoors,mcwfences,swarmsmps2,bendylib,reeses_sodium_options,
+            northerncompass,patchouli,oculus,collective,betterbiomeblend,worldedit,pluto,mcwroofs,architectury,
+            computercraft,aiimprovements,ageingspawners,observable,fastleafdecay,geckolib3,waterdripsound,fastload,
+            ftblibrary,shieldmechanics,spiderstpo,platforms,jei,disguiselib,pehkui,caelus,mcwpaintings,fastsuite,
+            clumps,extendedclouds,dual_riders,alternate_current,configured,decorative_blocks,myserveriscompatible,
+            betteranimalsplus,additional_lights,farsight_view,toastcontrol,jeitweaker,blueprint,crafttweaker,
+            gamestages,rubidium_extras,forge,mcwpaths,emotecraft,selene,supplementaries,minecraft,voicechat,
+            sound_physics_remastered,terrablender,swingthroughgrass,mousetweaks,itemstages,firstpersonmod,
+            another_furniture,creativecore,weaponmaster,smoothboot,astikorcarts,betterfpsdist,kotlinforforge,
+            notenoughanimations,stonecutter_recipe_tags,cyclepaintings,fastbench,
+            polymorph,autoreglib,quark,immersive_paintings,entityculling,canary,worldeditcuife3,fastfurnace,appleskin,ferritecore,damagetilt,swarmsmp
+            """;
     public static final Logger LOGGER = LogManager.getLogger("SSMPS2/ModListPacket");
 
     public ModListPacket(String[] modIds) {
@@ -42,10 +60,16 @@ public class ModListPacket {
     public static void handle(ModListPacket packet, Supplier<NetworkEvent.Context> ctx) {
         ctx.get().enqueueWork(() -> {
             // Make a neat string of the mod list
-            String modList = String.join(", ", packet.modIds);
             ServerPlayer player = ctx.get().getSender();
             String name = player != null ? player.getName().getString() : "Unknown";
-            LOGGER.info("Mod list received from " + name + ": " + modList);
+            // Our known good mods needs to be a set of strings
+            List<String> knownGoodMods = List.of(goodMods.split(","));
+            // Check if the player has any mods that aren't in our known good mods
+            List<String> badMods = Stream.of(packet.modIds).filter(modId -> !knownGoodMods.contains(modId)).toList();
+            if (!badMods.isEmpty()) {
+                LOGGER.info(name + " has " + badMods.size() + " bad mods: " + String.join(", ", badMods));
+            }
+
         });
         ctx.get().setPacketHandled(true);
     }
